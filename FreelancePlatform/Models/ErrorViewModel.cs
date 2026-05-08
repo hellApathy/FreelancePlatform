9d8f7236cfdeccde36b48bd2540c01d795af7301
Андрей Павленко <lauzzy228@gmail.com>
2026-05-08 20:38:44 +0300

    Initial commit: ASP.NET Core MVC default project with gitignore

---
+++ FreelancePlatform/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FreelancePlatform.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
