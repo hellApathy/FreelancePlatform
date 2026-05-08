38a10ed31aec8e2288697bb0606da3a58dbbb8a7
Андрей Павленко <lauzzy228@gmail.com>
2026-05-08 21:08:57 +0300

    Update Program.cs to use Controllers and Clean Architecture DI

--- FreelancePlatform.API/Program.cs
+++ FreelancePlatform.API/Program.cs
@@ -1,41 +1,37 @@
+using FreelancePlatform.DAL;
+using FreelancePlatform.DAL.Repositories;
+using FreelancePlatform.Domain.Interfaces;
+using FreelancePlatform.BLL.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// 1. Добавляем поддержку контроллеров (важно для п.8 вашего задания)
+builder.Services.AddControllers();
+
+// 2. Настройка БД (SQLite для простоты разработки)
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite("Data Source=freelance.db"));
+
+// 3. Настройка IoC (Dependency Injection) - это п.5 вашего задания!
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<OrderService>();
+
+// 4. Оставляем OpenAPI для удобного тестирования в браузере
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Настройка HTTP конвейера
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.MapOpenApi(); 
 }
 
 app.UseHttpsRedirection();
+app.UseAuthorization();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+// Подключаем маршрутизацию к нашим контроллерам
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
